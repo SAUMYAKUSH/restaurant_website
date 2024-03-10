164fdb8e9680fbbd0bf593fa02d5581ef23f2106
SAUMYAKUSH <saumyakushwaha803@gmail.com>
2024-03-10 18:47:41 +0530

    2nd commit

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -8,6 +8,14 @@ import { clearCart } from "../utils/CartSlice";
     const handleClearCart=()=>{
         dispatch(clearCart());
     }
+    console.log(cartItem)
+    let total = cartItem.reduce((sum,current)=>{
+    console.log(current.card.info)
+    return (sum + Number(current?.card?.info?.price?current?.card?.info?.price:current?.card?.info?.defaultPrice))}, 0
+   );
+   total/=100;
+ 
+
     return<div className="text-center m-4 p-4">
         <h1 className="text-xl font-bold"> Cart </h1>
         <div className="w-6/12 m-auto">
@@ -15,6 +23,7 @@ import { clearCart } from "../utils/CartSlice";
          {cartItem.length=== 0 && <h1>Cart is empty</h1>}
             <ItemList items={cartItem}/>
         </div>
+        <p className="text-lg font-bold"> Total - {`${total.toFixed(2)}`}</p>
     </div>
  }
 
